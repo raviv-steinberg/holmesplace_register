a84cd4ea8ca7fee8e427515b3cbecff4609f8b39
raviv-steinberg <raviv.steinberg@gmail.com>
2024-05-03 12:51:18 +0300

    Add 'FITBALL' LessonType.

--- src/enums/lesson_type.py
+++ src/enums/lesson_type.py
@@ -1,4 +1,5 @@
 from enum import Enum
+from typing import Any
 
 
 class LessonType(Enum):
@@ -6,6 +7,7 @@ class LessonType(Enum):
     Represents different types of lessons.
     """
     FELDENKRAIS = "פלדנקרייז"
+    FITBALL = "פיטבול"
     YOGA = "יוגה"
     HEALTH_EXERCISE = "התעמלות בריאותית"
     SPINNING = "ספינינג"
@@ -14,7 +16,7 @@ class LessonType(Enum):
     DYNAMIC_DESIGN = "עיצוב דינאמי"
 
     @staticmethod
-    def get_hebrew_name(english_name: str) -> str:
+    def get_hebrew_name(english_name: str) -> Any:
         """
         Get the Hebrew name of a lesson type based on its English name.
         :param english_name: The English name of the lesson type.
@@ -25,5 +27,4 @@ class LessonType(Enum):
         for lesson_type in LessonType:
             if lesson_type.name == english_name:
                 return lesson_type.value
-
         raise ValueError(f"No Hebrew name found for lesson type '{english_name}'")
